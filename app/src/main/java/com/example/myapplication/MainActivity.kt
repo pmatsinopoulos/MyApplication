701ca3cef4713127f2b225963654c7c6e636c88d
Panos Matsinopoulos <panos@mixlr.com>
2023-05-31 15:47:19 +0300

    Do some renames with refactoring

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,23 +5,17 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val greetingTextView = findViewById<TextView>(R.id.tvHello)
-        val inputField = findViewById<EditText>(R.id.tvName)
-        var submitButton = findViewById<Button>(R.id.sendName)
+        val inputField = findViewById<EditText>(R.id.etName)
+        var submitButton = findViewById<Button>(R.id.btnSubmit)
         submitButton.setOnClickListener {
             val enteredName = inputField.text
             val message = "Welcome $enteredName"
