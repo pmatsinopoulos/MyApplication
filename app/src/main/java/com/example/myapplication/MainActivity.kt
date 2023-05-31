4234430bef61613a772a41dff8ef1b5ca7a7f939
Panos Matsinopoulos <panos@mixlr.com>
2023-05-31 15:41:06 +0300

    Add button click listener

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,6 +19,14 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val greetingTextView = findViewById<TextView>(R.id.tvHello)
+        val inputField = findViewById<EditText>(R.id.tvName)
+        var submitButton = findViewById<Button>(R.id.sendName)
+        submitButton.setOnClickListener {
+            val enteredName = inputField.text
+            val message = "Welcome $enteredName"
+            greetingTextView.text = message
+        }
     }
 }
 
